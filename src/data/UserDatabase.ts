38bc98342439683557bc7114c56ac8e1f1f5b44b
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-05-19 16:50:35 -0300

    adiciona endpoint fazer amizade

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -1,15 +1,13 @@
 import { BaseDataBase } from "./BaseDatabase";
 
-export class UserDatabase extends BaseDataBase{
+export class UserDatabase extends BaseDataBase {
     tableName: string = "labook5_users";
 
-    public async signup(id:string ,name:string, email: string, password:string){
+    public async signup(id: string, name: string, email: string, password: string) {
         return await super.getConnection().raw(`
             INSERT INTO ${this.tableName} (user_id, name, email, password)
             VALUES('${id}', '${name}', '${email}', '${password}')
         `);
     }
-
-
 
 }
--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { UserController } from "../controller/UserController";
+import { FriendshipController } from "../controller/FollowController"
 
 export const userRouter = express.Router();
 
 userRouter.post("/signup", new UserController().signup);
+
+
+// Follow routes
+userRouter.post("/friendship", new FriendshipController().addFriend)
 
