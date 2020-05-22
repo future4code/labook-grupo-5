fdb0573d75a9631fba5a4553cfad740afd19f062
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-05-22 15:29:34 -0300

    adiciona refatoração dos endpoints fazer e desfazer amizade, com tratamento dos erros não seguir/deixar de seguir  a mesma pessoa duas vezes e não aceitar campos vazios

--- src/controller/FollowController.ts
+++ src/controller/FollowController.ts
@@ -11,17 +11,18 @@ export class FriendshipController {
             const followerToken = req.headers.authorization as string
             const followedId = req.body.followedId
 
-            if (!followedId || !followerToken) {
+            const userConnectionDB = new UserConnectionDB()
+
+            if (!followedId || !followerToken || followedId === "") {
                 throw new Error("Dados de usuário inválidos")
             }
 
             const authenticator = new Authenticator()
             const followerId = authenticator.verify(followerToken)
 
-            const userConnectionDB = new UserConnectionDB()
             await userConnectionDB.follow(followerId.id, followedId)
 
-            res.status(200).send({ message: "Usuário seguido com sucesso!" })
+            res.status(200).send({ message: "PARABÉNS, você tem um novo amigo!" })
 
         } catch (err) {
             res.status(400).send({ message: err.message })
@@ -37,13 +38,14 @@ export class FriendshipController {
             const followerToken = req.headers.authorization as string
             const unfollowedId = req.body.userToUnfollowId
 
-            if(!unfollowedId || !followerToken) {
-                throw new Error ("Dados inválidos")
+            if (!unfollowedId || !followerToken || unfollowedId === "") {
+                throw new Error("Dados inválidos")
             }
 
             const followerId = new Authenticator().verify(followerToken)
             await new UserConnectionDB().unfollow(followerId.id, unfollowedId)
 
+            res.status(200).send({ message: "Amizade desfeita!" })
 
         } catch (err) {
             res.status(400).send({ message: err.message })
--- src/data/UserConnectionDB.ts
+++ src/data/UserConnectionDB.ts
@@ -1,22 +1,44 @@
 import { BaseDataBase } from './BaseDatabase'
 
 export class UserConnectionDB extends BaseDataBase {
-     tableName: string = "labook5_friendship"
+    tableName: string = "labook5_friendship"
 
-    public async follow(followerId:string, followedId: string): Promise<void> {
-        await this.getConnection()
-        .insert({
-            user_a_id: followerId,
-            user_b_id:followedId
-        })
-        .into(this.tableName)
+    public async follow(followerId: string, followedId: string): Promise<void> {
+        const result = await this.getConnection()
+            .raw(`SELECT friendship_id FROM ${this.tableName} 
+            WHERE (user_a_id = "${followerId}" AND user_b_id = "${followedId}")
+            OR (user_a_id = "${followedId}" AND user_b_id = "${followerId}")
+            `)
+
+        if (result[0][0]) {
+            throw new Error("Amizade já existe.")
+        } else {
+            await this.getConnection()
+                .insert({
+                    user_a_id: followerId,
+                    user_b_id: followedId
+                })
+                .into(this.tableName)
+        }
     }
-    
-    public async unfollow(followedId: string, followerId: string): Promise<void> {
-        await this.getConnection()
-        .delete()
-        .from(this.tableName)
-        .where({user_b_id:followedId})
-        .andWhere({user_a_id: followerId})
+
+    public async unfollow(followerId: string, followedId: string): Promise<void> {
+        const result = await this.getConnection()
+            .raw(`SELECT friendship_id FROM ${this.tableName} 
+            WHERE (user_a_id = "${followerId}" AND user_b_id = "${followedId}")
+            OR (user_a_id = "${followedId}" AND user_b_id = "${followerId}")
+            `)
+
+        if (!result[0][0]) {
+            throw new Error("Não é possível desfazer uma amizade que não existe.")
+        } else {
+            await this.getConnection()
+                .delete()
+                .from(this.tableName)
+                .where({ user_b_id: followedId, user_a_id: followerId })
+                .orWhere({ user_a_id: followerId, user_b_id: followedId })
+                .into(this.tableName)
+
+        }
     }
 }
